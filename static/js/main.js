ed010815e488e5b4b19e3e76428cb7732794daff
Ariunbayar Baterdene <ariunbayar.b@gmail.com>
2014-08-22 18:48:04 +0800

    Crash feature

--- static/js/main.js
+++ static/js/main.js
@@ -4,12 +4,13 @@
 var Fall = {
     is_paused: true,
     is_ready: false,
-    man: { ready: true, cur_col: 4},
+    man: { ready: true, cur_col: 4 },
     column: 8,   // default width
     row:    12,  // default height
     cell: {width: 50, height: 50},
 
-    tick_speed: 100,  // milliseconds
+    tick_speed: 50,  // milliseconds
+    fall_speed: 500,
 
     layers: {},
     height: 0,
@@ -41,9 +42,10 @@ var Fall = {
         setInterval(Fall.tick, Fall.tick_speed);
         Fall.height = Fall.layers.front.height();
         Fall.width = Fall.layers.front.width();
-        $("#man").css({
-            'margin-left': Fall.man.cur_col * Fall.cell.width,
-            'margin-top': parseInt(Fall.row / 3) * Fall.cell.height
+        Fall.man.el = $("#man");
+        Fall.man.el.css({
+            left: Fall.man.cur_col * Fall.cell.width,
+            top: parseInt(Fall.row / 3) * Fall.cell.height
             }
         );
         },
@@ -80,10 +82,16 @@ var Fall = {
                 var column = ~~(Math.random() * 8) + 1;
                 var item = Fall.add_item(layer, column);
                 // TODO by height and layer speed
-                var speed = 1500 / layer.speed;
+                var speed = Fall.fall_speed / layer.speed;
                 Fall.move(item, speed);
             }
         }
+        Fall.layers.front.children().each(function(i, _el){
+            if (Fall.man.el.is(_el)) return;
+            if (Fall.is_colliding(_el, Fall.man.el)) {
+                Fall.crash();
+            }
+        });
         Fall.update();
 
         Fall.is_ready = true;
@@ -95,13 +103,33 @@ var Fall = {
             });
         },
 
+    is_colliding: function(_el1, _el2){
+        var el1 = $(_el1), el2 = $(_el2);
+        var el1_pos = el1.position(),
+            el2_pos = el2.position();
+        el1_pos.bottom = el1_pos.top  + el1.height();
+        el1_pos.right  = el1_pos.left + el1.width();
+        el2_pos.bottom = el2_pos.top  + el2.height();
+        el2_pos.right  = el2_pos.left + el2.width();
+        if (el1_pos.left   < el2_pos.right &&
+            el1_pos.right  > el2_pos.left &&
+            el1_pos.top    < el2_pos.bottom &&
+            el1_pos.bottom > el2_pos.top) {
+            $('#p1').css({top: el1_pos.top, left: el1_pos.left});
+            $('#p2').css({top: el1_pos.bottom, left: el1_pos.left});
+            $('#p3').css({top: el1_pos.top, left: el1_pos.right});
+            $('#p4').css({top: el1_pos.bottom, left: el1_pos.right});
+            return true;
+        } else {
+            return false;
+        }
+        },
+
     left: function() {
         Fall.man.ready = false;
-        var cur_left = parseInt($("#man").css('margin-left'));
         Fall.man.cur_col--;
-        $("#man").animate({
-            'margin-left': Fall.man.cur_col * Fall.cell.width
-            }, 300, "linear", function() {
+        var css = { left: Fall.man.cur_col * Fall.cell.width };
+        Fall.man.el.animate(css, 300, "linear", function() {
                 Fall.man.ready = true;
             }
         );
@@ -109,11 +137,9 @@ var Fall = {
 
     right: function() {
         Fall.man.ready = false;
-        var cur_left = parseInt($("#man").css('margin-left'));
         Fall.man.cur_col++;
-        $("#man").animate({
-            'margin-left': Fall.man.cur_col * Fall.cell.width
-            }, 300, "linear", function() {
+        var css = { left: Fall.man.cur_col * Fall.cell.width }
+        Fall.man.el.animate(css, 300, "linear", function() {
                 Fall.man.ready = true;
             }
         );
@@ -124,7 +150,8 @@ var Fall = {
         },
 
     crash: function(){
-        // TODO
+        Fall.man.el.css('background-color', 'red');
+        Fall.toggle_pause();
         },
 
     finish: function(){
